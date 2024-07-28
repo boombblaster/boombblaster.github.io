f764744af8b07bead67c3bdd5b17ffde20400da1
raffzx <raffifauzan817@gmail.com>
2024-07-29 01:23:27 +0700

    main.js

--- scripts/main.js
+++ scripts/main.js
@@ -17,3 +17,15 @@ window.onbeforeunload = () => {
         form.reset();
     }
 };
+
+
+// Bergulir Smooth
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
